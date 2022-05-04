a0d44b98bbb42ce86a61555f474e5be8b8482198
Asher Christian <asher.christian@gmail.com>
2022-05-04 13:26:20 -0700

    work

--- NewProject.py
+++ NewProject.py
@@ -6,17 +6,24 @@ def bases(cls):
         for direct_base in cls.__bases__:
             for base in bases(direct_base):
                 yield base
+def multElements(iterable):
+    sum = 1
+    for x in iterable:
+        sum *= x
+    return x
 
 class organism:
     instances = []
     aliveInstances = []
-    def __init__(self, name = 'NaN', dimensions = (1,1,1), parents = (None, None), position = [0,0], data = {}, alive = True):
+    def __init__(self, name = 'NaN', dimensions = (1,1,1), parents = (None, None), position = [0,0], data = {}, alive = True, energy = 0):
             self.name = name
             self.dimensions = dimensions
             self.parents = parents
             self.position = position
+            self.energy = energy
             self.data = data
             self.alive = alive
+            self.energyStore = multElements(self.dimensions)
             for x in bases(type(self)):
                 x.instances.append(self)
                 if self.alive == True:
@@ -28,14 +35,21 @@ class organism:
         self.alive == False
         for x in bases(type(self)):
             x.aliveInstances.remove(self)
+    def isAlive(self):
+        return self.alive
     def getName(self):
         return self.name
     def getPos(self):
         return (self.position[0], self.position[1])
     def move(self, newPosition):
         self.position = newPosition
+    def consume(self, organism):
+        organism.kill()
+        self.energy += organism.energyStore
+        organism.energyStore = 0
     def __str__(self):
         return self.name
+    
 class bean(organism):
     instances = []
     aliveInstances = []
@@ -109,14 +123,14 @@ class world:
         indexes = []
         for i in self.plot:
             for x in self.plot[i][key]:
-                if x.getPos() != i:
+                if x.getPos() != i or x.isAlive() == False:
                     indexes.append((i, x))
         for i in indexes:
             self.plot[i[0]][key].remove(i[1])
 
 
 def main():
-    Asher = organism('Asher')
+    Tree = tree('Asher')
     beany = bean('beany')
     Earth = world('earth', data = tileInfo(bean = [], tree = []))
     Earth.updateLocs(organism.aliveInstances)

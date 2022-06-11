1f103a203df869ffc110cce54d87bf768e4c1ec6
Asher Christian <asher.christian@gmail.com>
2022-06-11 13:51:36 -0700

    changes

--- transform.py
+++
@@ -1,8 +0,0 @@
-import regex
-import string
-
-words = string.ascii_letters
-print(words)
-with open("C://Users//Bobeta//Documents//frankenstein.txt", 'r') as book:
-    book = book.read()
-    print(book)
